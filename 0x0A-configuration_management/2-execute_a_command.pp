f0821fa3c8ff48a34ea9b267a39e4d2b9d8c5340
root <bustlezach001@gmail.com>
2023-04-23 07:55:56 -0700

    task

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,8 @@
 #!usr/bin/pup
 # Execute the process kill command using pkill command
 
-exec {'killmenow':
-	command	=> 'pkill',
-	path	=> '/usr/bin:/usr/sbin:/bin'
+exec { 'kill':
+  command => 'pkill -f killmenow',
+  path    => '/usr/bin:/usr/sbin:/bin',
 }
+
