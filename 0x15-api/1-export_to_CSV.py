e5d728184dc94f62bfcf8e0e402716e4ac45fc16
bustlezach <bustlezach001@gmail.com>
2023-07-24 20:14:59 -0700

    task

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,24 +1,29 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
+
 """
 This Python script uses the REST API,
 for a given employee ID, returns information
 about his/her TODO list progress and stores it in a CSV file.
 """
+
 import csv
 import requests
 from sys import argv
 
+def fetch_todo_progress(user_id):
+    """
+    Fetches the user's TODO list progress and stores it in a CSV file.
 
-if __name__ == "__main__":
-    """Fetch the user ID from command-line arguments"""
-    user_id = argv[1]
+    Args:
+        user_id (str): The employee ID to fetch TODO list for.
 
+    Returns:
+        None
+    """
     url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
     res = requests.get(url)
     user_dict = res.json()
     username = user_dict.get("username")
-
 
     url = f"https://jsonplaceholder.typicode.com/users/{user_id}/todos"
     res = requests.get(url)
@@ -27,9 +32,7 @@ if __name__ == "__main__":
     field_names = ['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE']
     result = []
 
-
     for task in tasks:
-    """Prepare the data for the CSV file"""
         result.append({
             'USER_ID': user_id,
             'USERNAME': username,
@@ -37,9 +40,11 @@ if __name__ == "__main__":
             'TASK_TITLE': task.get('title')
         })
 
-
     with open(f'{user_id}.csv', 'w', newline='') as csvfile:
-    """Write the data to a CSV file named after the user_id"""
         writer = csv.DictWriter(csvfile, fieldnames=field_names)
         writer.writeheader()
         writer.writerows(result)
+
+if __name__ == "__main__":
+    user_id = argv[1]
+    fetch_todo_progress(user_id)
