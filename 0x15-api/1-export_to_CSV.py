8ca25bf3dd2a007214ecadb4351e2353ec1623f4
bustlezach <bustlezach001@gmail.com>
2023-07-24 19:59:33 -0700

    task

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -7,29 +7,36 @@ import requests
 from sys import argv
 
 
-def if __name__ == "__main__":
+if __name__ == "__main__":
+    """Fetch the user ID from command-line arguments"""
     user_id = argv[1]
+
     url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
     res = requests.get(url)
     user_dict = res.json()
     username = user_dict.get("username")
 
+
     url = f"https://jsonplaceholder.typicode.com/users/{user_id}/todos"
     res = requests.get(url)
     tasks = res.json()
 
-    field_names = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
+    field_names = ['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE']
     result = []
 
+
     for task in tasks:
+    """Prepare the data for the CSV file"""
         result.append({
-            "USER_ID": user_id,
-            "USERNAME": username,
-            "TASK_COMPLETED_STATUS": task.get("completed"),
-            "TASK_TITLE": task.get("title"),
+            'USER_ID': user_id,
+            'USERNAME': username,
+            'TASK_COMPLETED_STATUS': task.get('completed'),
+            'TASK_TITLE': task.get('title')
         })
 
-    with open(f"{user_id}.csv", "w", newline="") as csvfile:
+
+    with open(f'{user_id}.csv', 'w', newline='') as csvfile:
+    """Write the data to a CSV file named after the user_id"""
         writer = csv.DictWriter(csvfile, fieldnames=field_names)
         writer.writeheader()
         writer.writerows(result)
