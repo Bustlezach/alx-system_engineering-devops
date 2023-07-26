d4bbc2ab0dabaa555b21085457f4d9b6095341e9
bustlezach <bustlezach001@gmail.com>
2023-07-26 15:33:55 -0700

    task

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,55 +1,30 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 """
-This Python script uses the REST API,
-for a given employee ID, returns information
-about his/her TODO list progress.
+Python script to export data in the JSON format
 """
+from json import dump
+from requests import get
 
-import requests
-from sys import argv
-import json
 
-def fetch_todo_progress(user_id):
-    """
-    Fetches the user's TODO list progress.
+if __name__ == '__main__':
+    url = 'https://jsonplaceholder.typicode.com/users/'
+    message = get(url)
+    users = message.json()
 
-    Args:
-        user_id (str): The employee ID to fetch TODO list for.
-
-    Returns:
-        dict: Dictionary containing the employee's TODO list progress.
-    """
-    url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
-    res = requests.get(url)
-    user_dict = res.json()
-    username = user_dict.get("username")
-
-    url = f"https://jsonplaceholder.typicode.com/users/{user_id}/todos"
-    res = requests.get(url)
-    tasks = res.json()
-
-    task_list = []
-    for task in tasks:
-        task_list.append({
-            "username": username,
-            "task": task.get('title'),
-            "completed": task.get('completed')
-        })
-
-    return {user_id: task_list}
-
-def main():
-    """
-    Main function to fetch all employees' TODO list progress and store it in a JSON file.
-    """
-    all_employees_data = {}
-    for user_id in argv[1:]:
-        employee_data = fetch_todo_progress(user_id)
-        all_employees_data.update(employee_data)
-
+    dict = {}
+    for user in users:
+        user_id = user.get('id')
+        username = user.get('username')
+        url = f'https://jsonplaceholder.typicode.com/users/{user_id}/todos/'
+        message = get(url)
+        tasks = message.json()
+        dict[user_id] = []
+        for task in tasks:
+            dict[user_id].append({
+                                        "task": task.get('title'),
+                                        "completed": task.get('completed'),
+                                        "username": username
+                                        })
     with open('todo_all_employees.json', 'w') as file:
-        json.dump(all_employees_data, file)
-
-if __name__ == "__main__":
-    main()
+        dump(dict, file)
