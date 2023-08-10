9398c7ffcfb2d1e64d5e3afb19488ca1e4d69c36
bustlezach <bustlezach001@gmail.com>
2023-08-10 15:04:01 -0700

    task

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-
-"""
-This script queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit
-"""
+"""Function to print hot posts on a given Reddit subreddit."""
 import requests
 
 
@@ -11,9 +8,10 @@ def top_ten(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     headers = {'User-Agent': 'linux:0x16.api.advanced:v1.0.0 (by /u/bustlezee)'}
     params = {'limit': 10}
-    message = requests.get(url, headers=headers, params=params, allow_redirects=False)
-	if message.status_code == 404:
-		print('None')
-		return
-    res = message.json().get('data')
+    message = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if message.status_code == 404:
+        print("None")
+        return
+    res = message.json().get("data")
     [print(i.get("data").get("title")) for i in res.get("children")]
