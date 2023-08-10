3152508fb31b89650045f42466828ed180eb208a
bustlezach <bustlezach001@gmail.com>
2023-08-10 14:45:36 -0700

    task

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,13 +9,13 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """Returns the number of subscribers"""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'linux:0x16.api.advanced:v1.0.0 (by /u/bustlezee)'}
-
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bustlezee)"
+    }
     message = requests.get(url, headers=headers, allow_redirects=False)
     if message.status_code == 404:
         return 0
-
-    response = message.json().get('data')
-	return response.get('subscribers')
+    res = message.json().get("data")
+    return res.get("subscribers")
