de34632f167dd3fa16431cb93f9b67453cae7956
bustlezach <bustlezach001@gmail.com>
2023-08-10 15:28:31 -0700

    task

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
-
 """
-This script queries the Reddit API and returns a list containing the titles of all hot articles for a given subreddit.
+This script queries the Reddit API and returns a list
+containing the titles of all hot articles for a given subreddit.
 """
 
 import requests
 
+def recurse(subreddit, hot_list=None, after=None):
+    """This function recursively prints the article titles."""
+    if hot_list is None:
+        hot_list = []
 
-def recurse(subreddit, hot_list=[], after=None):
-    """This function recursively print the article titles"""
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     headers = {'User-Agent': 'Custom User-Agent'}
     params = {'limit': 25, 'after': after}
 
-    message = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    message = requests.get(
+        url, headers=headers, params=params, allow_redirects=False
+    )
 
     if message.status_code == 404:
         return None
