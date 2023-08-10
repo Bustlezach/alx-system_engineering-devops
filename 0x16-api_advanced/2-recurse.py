8be2af714ae5eeeb40a1a26c3f0f6be492ca8947
bustlezach <bustlezach001@gmail.com>
2023-08-10 15:36:29 -0700

    task

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,34 +1,33 @@
 #!/usr/bin/python3
-
 """
-This script queries the Reddit API and returns a list containing the titles of all hot articles for a given subreddit.
+This script queries the Reddit API and returns a list
+containing the titles of all hot articles for a given subreddit.
 """
-
 import requests
 
 
-def recurse(subreddit, hot_list=[], after=None):
-    """This function recursively print the article titles"""
+def recurse(subreddit, hot_list=[], after="", count=0):
+    """This function recursively prints the article titles."""
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {'User-Agent': 'Custom User-Agent'}
-    params = {'limit': 25, 'after': after}
-
-    message = requests.get(url, headers=headers, params=params, allow_redirects=False)
-
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /bustlezee)"
+    }
+    params = {
+        "after": after,
+        "count": count,
+        "limit": 100
+    }
+    message = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
     if message.status_code == 404:
         return None
 
-    res = message.json()
+    res = message.json().get("data")
+    after = res.get("after")
+    count += res.get("dist")
+    for i in res.get("children"):
+        hot_list.append(i.get("data").get("title"))
 
-    if 'data' not in res:
-        return None
-
-    data = res['data']
-    after = data['after']
-
-    for post in data['children']:
-        hot_list.append(post['data']['title'])
-
-    if after:
-        return recurse(subreddit, hot_list, after)
+    if after is not None:
+        return recurse(subreddit, hot_list, after, count)
     return hot_list
