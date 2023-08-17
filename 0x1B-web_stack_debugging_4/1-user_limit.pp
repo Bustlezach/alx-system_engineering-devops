e91efa1291d399563ad957e1178ed825dd7a640a
bustlezach <bustlezach001@gmail.com>
2023-08-17 04:37:54 -0700

    task

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,11 @@
+# Enables holberton user's to login and open files without error.
+
+exec { 'increase-user-limit':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+exec { 'increase-user-soft-file-limit':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
